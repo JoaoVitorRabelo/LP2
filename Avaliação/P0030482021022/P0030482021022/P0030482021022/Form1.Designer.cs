15cfe302b4be7f900bc82629a32358e19ebc2b22
JoaoVitorRabelo <78799931+JoaoVitorRabelo@users.noreply.github.com>
2021-05-18 12:24:36 -0300

    Add files via upload

---
+++ Avaliação/P0030482021022/P0030482021022/P0030482021022/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace P0030482021022
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lboxRes = new System.Windows.Forms.ListBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnVerificar.Location = new System.Drawing.Point(120, 123);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(155, 82);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // lboxRes
+            // 
+            this.lboxRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lboxRes.FormattingEnabled = true;
+            this.lboxRes.ItemHeight = 24;
+            this.lboxRes.Location = new System.Drawing.Point(408, 26);
+            this.lboxRes.Name = "lboxRes";
+            this.lboxRes.Size = new System.Drawing.Size(380, 412);
+            this.lboxRes.TabIndex = 1;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnLimpar.Location = new System.Drawing.Point(120, 221);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(155, 82);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lboxRes);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox lboxRes;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ Avaliação/P0030482021022/P0030482021022/P0030482021022/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Collections;
+
+namespace P0030482021022
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            double[,] Valores = new double[6, 4];
+            double[] TotalMes = new double[6];
+            string valor;
+            double totalMeses = 0;
+
+            for (int x = 0; x < 2; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    valor = Interaction.InputBox("Digite o valor da semana " + (y + 1) + " Do Mês: " + (x + 1), "Entrada de dados");
+                    if (valor == "")
+                    {
+                        MessageBox.Show("Campo está vazio");
+                        y--;
+                    }
+                    else if (double.TryParse(valor, out Valores[x, y]))
+                    {
+                        double.TryParse(valor, out Valores[x, y]);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Digite apenas números!");
+                        y--;
+                    }
+                }
+            }
+
+            for (int x = 0; x < 2; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    TotalMes[x] = TotalMes[x] + Valores[x, y];
+                }
+                totalMeses = totalMeses + TotalMes[x];
+            }
+
+            for (int x = 0; x < 2; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    lboxRes.Items.Add("Total do Mês: " + (x + 1) + " Semana: " + (y + 1) + ": R$" + Valores[x, y].ToString("N2"));
+                }
+                lboxRes.Items.Add(">>Total Mês: R$" + TotalMes[x].ToString("N2"));
+                lboxRes.Items.Add("...............................");
+            }
+            lboxRes.Items.Add("Total Geral: R$" + totalMeses.ToString("N2"));
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            lboxRes.Items.Clear();
+        }
+    }
+}
